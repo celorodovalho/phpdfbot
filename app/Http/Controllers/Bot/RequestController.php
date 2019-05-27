0873fb2d3fbe5f384b614bc76d6d8b64675dc323
Marcelo Rodovalho <marcelo2208@gmail.com>
2019-05-27 16:24:50 +0200

    GMail API

--- app/Http/Controllers/Bot/RequestController.php
+++ app/Http/Controllers/Bot/RequestController.php
@@ -14,14 +14,9 @@ class RequestController extends Controller
     public function process(): string
     {
         $messages = LaravelGmail::message()
-            ->raw('list:nvagas@googlegroups.com')
-            ->raw('list:leonardoti@googlegroups.com')
-            ->raw('list:clubinfobsb@googlegroups.com')
-            ->to('nvagas@googlegroups.com')
-            ->to('vagas@noreply.github.com')
-            ->to('clubinfobsb@googlegroups.com')
-            ->to('leonardoti@googlegroups.com')
-//            ->fromThese()
+            ->raw('(list:nvagas@googlegroups.com OR list:leonardoti@googlegroups.com ' .
+                'OR list:clubinfobsb@googlegroups.com OR to:nvagas@googlegroups.com OR to:vagas@noreply.github.com ' .
+                'OR to:clubinfobsb@googlegroups.com OR to:leonardoti@googlegroups.com)')
             ->unread()
             ->preload();
         dump($messages);
