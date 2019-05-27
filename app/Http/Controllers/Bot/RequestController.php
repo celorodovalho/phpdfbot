b3968b012068b25523bfe2ab626d7b61822f5f5b
Marcelo Rodovalho <marcelo2208@gmail.com>
2019-05-28 00:10:38 +0200

    [UPDATE] Retrieving e-mails and preparing to send

--- app/Http/Controllers/Bot/RequestController.php
+++ app/Http/Controllers/Bot/RequestController.php
@@ -32,7 +32,8 @@ class RequestController extends Controller
     {
         $messages = $this->getMessages();
 dump($messages->first()->getBody());
-dump($messages->first()->getAttachments(true));
+echo "<img src='data:image/jpeg;base64,{$messages->first()->getAttachments(true)}'>";
+//dump($messages->first()->getAttachments(true));
         /** @var Mail $message */
         foreach ($messages as $message) {
             /** TODO: Format message here */
