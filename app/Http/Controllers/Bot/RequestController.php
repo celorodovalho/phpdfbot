b27a4e87ba31570dec52cc91f6e71d06813571e1
Marcelo Rodovalho <marcelo2208@gmail.com>
2019-05-27 17:20:02 +0200

    GMail API

--- app/Http/Controllers/Bot/RequestController.php
+++ app/Http/Controllers/Bot/RequestController.php
@@ -10,29 +10,18 @@ use Telegram;
 class RequestController extends Controller
 {
     /**
-     * (list:nvagas@googlegroups.com OR list:leonardoti@googlegroups.com OR list:clubinfobsb@googlegroups.com OR to:nvagas@googlegroups.com OR to:vagas@noreply.github.com OR to:clubinfobsb@googlegroups.com OR to:leonardoti@googlegroups.com) is:unread
+     * @return string
      */
     public function process(): string
     {
-//        $messages = LaravelGmail::message()
-//            ->raw('(list:nvagas@googlegroups.com OR list:leonardoti@googlegroups.com OR list:clubinfobsb@googlegroups.com OR to:nvagas@googlegroups.com OR to:vagas@noreply.github.com OR to:clubinfobsb@googlegroups.com OR to:leonardoti@googlegroups.com) is:unread')
-////            ->raw('(list:nvagas@googlegroups.com OR list:leonardoti@googlegroups.com ' .
-////                'OR list:clubinfobsb@googlegroups.com OR to:nvagas@googlegroups.com OR to:vagas@noreply.github.com ' .
-////                'OR to:clubinfobsb@googlegroups.com OR to:leonardoti@googlegroups.com)')
-////            ->unread()
-//            ->preload();
-//        dump($messages);
-//        dump($messages->all());
-//        dump(get_class_methods($messages));
-//        dump($messages->all()[0]->getSubject());
-//        dump($messages->all()[0]->getHtmlBody());
 
         /** @var \Dacastro4\LaravelGmail\Services\Message $messageService */
         $messageService = LaravelGmail::message();
         $threads = $messageService->service->users_messages->listUsersMessages('me', [
-            'q' => '(list:nvagas@googlegroups.com OR list:leonardoti@googlegroups.com OR list:clubinfobsb@googlegroups.com OR to:nvagas@googlegroups.com OR to:vagas@noreply.github.com OR to:clubinfobsb@googlegroups.com OR to:leonardoti@googlegroups.com) is:unread'
+            'q' => '(list:nvagas@googlegroups.com OR list:leonardoti@googlegroups.com ' .
+                'OR list:clubinfobsb@googlegroups.com OR to:nvagas@googlegroups.com OR to:vagas@noreply.github.com ' .
+                'OR to:clubinfobsb@googlegroups.com OR to:leonardoti@googlegroups.com) is:unread'
         ]);
-//        dump($threads->getThreads());
 
         $allMessages = $threads->getMessages();
         foreach ($allMessages as $message) {
