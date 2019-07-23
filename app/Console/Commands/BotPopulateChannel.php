f95f09b5ffc859cca0e7d97c0e2b0efab47ca950
Marcelo Rodovalho <marcelo2208@gmail.com>
2019-07-23 10:10:23 +0200

    [UPDATE] Add States filtering

--- app/Console/Commands/BotPopulateChannel.php
+++ app/Console/Commands/BotPopulateChannel.php
@@ -115,6 +115,7 @@ class BotPopulateChannel extends AbstractCommand
         // variacoes
         '"gerenciamento de projetos"',
         '"gerenciamento de negócios"',
+        //
     ];
 
     /**
@@ -123,6 +124,40 @@ class BotPopulateChannel extends AbstractCommand
     private $channel;
     private $appUrl;
     private $group;
+
+    /**
+     * Estados
+     * @var array
+     */
+    private $estadosBrasileiros = [
+        'AC' => 'Acre',
+        'AL' => 'Alagoas',
+        'AP' => 'Amapá',
+        'AM' => 'Amazonas',
+        'BA' => 'Bahia',
+        'CE' => 'Ceará',
+        'DF' => 'Distrito Federal',
+        'ES' => 'Espírito Santo',
+        'GO' => 'Goiás',
+        'MA' => 'Maranhão',
+        'MT' => 'Mato Grosso',
+        'MS' => 'Mato Grosso do Sul',
+        'MG' => 'Minas Gerais',
+        'PA' => 'Pará',
+        'PB' => 'Paraíba',
+        'PR' => 'Paraná',
+        'PE' => 'Pernambuco',
+        'PI' => 'Piauí',
+        'RJ' => 'Rio de Janeiro',
+        'RN' => 'Rio Grande do Norte',
+        'RS' => 'Rio Grande do Sul',
+        'RO' => 'Rondônia',
+        'RR' => 'Roraima',
+        'SC' => 'Santa Catarina',
+        'SP' => 'São Paulo',
+        'SE' => 'Sergipe',
+        'TO' => 'Tocantins'
+    ];
 
     /**
      * Execute the console command.
@@ -867,7 +902,7 @@ class BotPopulateChannel extends AbstractCommand
      */
     private function addHashtagFilters(string $message): string
     {
-        $pattern = '#(' . implode('|', $this->mustIncludeWords) . ')#i';
+        $pattern = '#(' . implode('|', ($this->mustIncludeWords + $this->estadosBrasileiros)) . ')#i';
         $pattern = str_replace('"', '', $pattern);
         if (preg_match_all($pattern, $message, $matches)) {
             $tags = [];
