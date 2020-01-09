78413e45a76a848ac73081942d57d7681a58e258
@se45ky <marcelo2208@gmail.com>
2020-01-09 14:12:31 +0100

    [FEAT] Start the development of web version

---
+++ app/Http/Controllers/Web/OpportunityController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class DefaultController
+ */
+class OpportunityController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
