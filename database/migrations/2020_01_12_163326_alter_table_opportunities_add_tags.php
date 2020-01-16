579edd205587780d32dc254b9ca991f5a7f9b3c1
@se45ky <marcelo2208@gmail.com>
2020-01-16 15:02:11 +0100

    [FEAT] Rename wrong migrations

--- database/migrations/2020_01_12_163326_alter_table_opportunities_add_tags.php
+++ database/migrations/2020_01_12_163326_alter_table_opportunities_add_tags.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTableOpportunitiesAddSource extends Migration
+class AlterTableOpportunitiesAddTags extends Migration
 {
     /**
      * Run the migrations.
