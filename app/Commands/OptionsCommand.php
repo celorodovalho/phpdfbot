6801030f9bdbea0474f45aa1efd4e6a00a219926
@se45ky <marcelo2208@gmail.com>
2020-01-28 13:11:04 +0100

    [FIX] Wrong return type
    Took 3 minutes

--- app/Commands/OptionsCommand.php
+++ app/Commands/OptionsCommand.php
@@ -30,7 +30,7 @@ class OptionsCommand extends Command
     /**
      * @inheritdoc
      */
-    public function handle(): void
+    public function handle()
     {
         Log::info('OPTIONS_COMMAND', [$this->arguments, $this->getArguments()]);
 
