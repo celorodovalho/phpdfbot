3d2d1148ee87c9b92d8baaa39b1870b8623cc28a
@se45ky <marcelo2208@gmail.com>
2020-02-19 22:48:02 +0100

    [FEAT] Improvements
    Took 8 minutes

--- .tevun/hooks/post-checkout.sh
+++ .tevun/hooks/post-checkout.sh
@@ -6,12 +6,12 @@ echo " ~> [hooks\post-checkout.sh] on [${1}, ${2}]"
 
 # echo "@ remove previous containers"
 # docker-compose rm -f
-if [ ".env" -ot ".env.stage" ]; then
-    echo "@ copy a new .env"
-    cp -f .env.stage .env
-    echo "@ generate a new key"
-    docker exec -it phpdfbot bash -c "php artisan key:generate"
-fi
+#if [ ".env" -ot ".env.stage" ]; then
+#    echo "@ copy a new .env"
+#    cp -f .env.stage .env
+#    echo "@ generate a new key"
+#    docker exec -it phpdfbot bash -c "php artisan key:generate"
+#fi
 echo "@ copy a new docker-compose"
 cp docker-compose.yml.stage docker-compose.yml
 # echo "@ start the app"
