5c79aee7a6ac259a92bacb95494ccbbb24def0a0
@se45ky <marcelo2208@gmail.com>
2020-03-03 14:28:34 +0100

    [FEAT] Add tinker web
    Took 32 minutes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 /**
  * Class AuthServiceProvider
@@ -29,6 +30,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewWebTinker', static function ($user = null) {
+            return env('DB_USERNAME') === request()->get('user');
+        });
     }
 }
